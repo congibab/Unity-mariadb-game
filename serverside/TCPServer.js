0571810e0a49aac0ce51863edad896e63c08f6f9
congibab <49512074+congibab@users.noreply.github.com>
2021-01-12 11:55:15 +0900

    TCP server event hendler sys is making

--- serverside/TCPServer.js
+++ serverside/TCPServer.js
@@ -1,32 +1,73 @@
+const { v4: uuidv4 } = require('uuid');
+
 var net = require('net');
 var colors = require('colors');
 
 var sockets = [];
 
+
 var server = net.createServer();
 
 server.on("connection", function (socket) {
     var remoteAddress = socket.remoteAddress + ":" + socket.remotePort;
-    console.log("new client connection is made %s".green, remoteAddress);
+    var thisClientId = uuidv4();
+    sockets[thisClientId] = socket;
 
-    sockets.push(socket);
+    console.log(Object.keys(sockets).length);
+    console.log("new client connection is made %s, UUID : %s".green, remoteAddress, thisClientId);
 
-    socket.on('data', function (d) {
+    socket.on('data', function (data) {
         socket.setEncoding('utf8');
-        console.log("Data from %s: %s".cyan, remoteAddress, d);
-        socket.write(d);
+        try {
+            var JSONdata = JSON.parse(data);
+            switch (JSONdata.type) {
+                case 'init':                    
+                    break;
+                case 'test':
+                    break;
+                default:
+                    break;
+            }
+            console.log("Data from %s: %s".cyan, remoteAddress, data);
+            jsonBroadcast('test');
+            //socket.write(data);
+        } catch (error) {
+            console.log('resived data type is not json'.yellow);
+            JSON_Broadcast('test broadcast');
+            //JSON_Send(socket, 'testtesttest');
+        }
     });
 
     socket.on('close', function () {
-        sockets.pop();
-        console.log("connection from %s closed", remoteAddress);
+        delete sockets[thisClientId];
+        console.log("connection from %s closed, UUID : %s".yellow, remoteAddress, thisClientId);
     });
 
     socket.on('error', function (err) {
-        console.log("Connection %s error: %s", remoteAddress, err.message);
+        console.log("Connection %s error: %s".red, remoteAddress, err.message);
     });
 });
 
 server.listen(5000, function () {
-    console.log("server listening to %j", server.address());
+    console.log("server listening to %j".green, server.address());
 });
+
+//========================================================
+//========================================================
+//========================================================
+function JSON_Send(socket, data){
+    try {
+        var dataJSON = JSON.stringify(data);
+        socket.write(dataJSON);
+        console.log('Send : %s'.blue, dataJSON);     
+    } catch (error) {
+        socket.write('test data');
+        console.log('this type is not JSON'.yellow);
+    }
+}
+
+function JSON_Broadcast(data) {
+    sockets.forEach(function each(socket) {
+        JSON_Send(socket, data);
+    });
+}
